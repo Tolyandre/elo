241d91e4160eb1be9294eee49675cddc72e35028
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-10-13 21:56:10 +0400

    Try to fix reloading of main page

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,4 +1,6 @@
-import type { Metadata } from "next";
+"use client";
+
+//import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PlayersProvider } from "./PlayersContext";
@@ -16,10 +18,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Board games elo",
-  description: "Put your board games score and calculate elo rating for players",
-};
+// export const metadata: Metadata = {
+//   title: "Board games elo",
+//   description: "Put your board games score and calculate elo rating for players",
+// };
 
 export default function RootLayout({
   children,
