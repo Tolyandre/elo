66bfe3a0d58293b7ccf97a55e8e1fc35ad4ed45b
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-10-31 01:21:48 +0400

    npm install

--- nextjs/components/navigation-bar.tsx
+++ nextjs/components/navigation-bar.tsx
@@ -35,7 +35,7 @@ export function NavigationBar() {
                 <NavigationMenuLink asChild>
                   <a
                     className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-4 no-underline outline-hidden transition-all duration-200 select-none focus:shadow-md md:p-6"
-                    href="/"
+                    href="./"
                   >
                     <div className="mb-2 text-lg font-medium sm:mt-4">
                       Рейтинг эло
