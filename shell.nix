5581a41b07ea48e1f8d3a92cdfb238a57625ed9c
Anatoley Buranov <2414704+Tolyandre@users.noreply.github.com>
2025-12-05 00:12:09 +0400

    shell.nix & .envrc

---
+++ shell.nix
@@ -0,0 +1,17 @@
+# https://github.com/nix-community/nix-direnv?tab=readme-ov-file#usage-example
+# don't foget to run `direnv allow`
+
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  hardeningDisable = [ "fortify" ];
+  
+  buildInputs = [
+    pkgs.libcap
+    pkgs.glibc.static
+    pkgs.go
+    pkgs.gcc
+  ];
+}
