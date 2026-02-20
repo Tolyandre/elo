ba7139d7df5b81b797e6ed0a41445f283f711984
Буранов Анатолий <an.buranov@tinkoff.ru>
2026-02-20 17:28:17 +0400

    chore(direnv): add git for mac

--- shell.nix
+++ shell.nix
@@ -9,6 +9,7 @@ pkgs.mkShell {
   hardeningDisable = [ "fortify" ];
 
   buildInputs = lib.optionals pkgs.stdenv.isLinux [ pkgs.libcap pkgs.glibc.static ] ++ [
+    pkgs.git # fix xcrun for mac (tool 'git' not found)
     pkgs.go
     pkgs.gcc
     pkgs.sqlc
