3a9a54c19a6df21eba5bbe92765532ccd59bfb6e
Буранов Анатолий <an.buranov@tinkoff.ru>
2026-02-23 02:28:26 +0400

    Prevent accident closing

--- nextjs/app/match/page.tsx
+++ nextjs/app/match/page.tsx
@@ -229,7 +229,11 @@ function EditMatchDialog({ match, onSuccess }: { match: Match; onSuccess: () => 
           Редактировать
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[500px] max-h-[90vh] overflow-y-auto">
+      <DialogContent
+        className="sm:max-w-[500px] max-h-[90vh] overflow-y-auto"
+        onInteractOutside={(e) => e.preventDefault()}
+        onEscapeKeyDown={(e) => e.preventDefault()}
+      >
         <DialogHeader>
           <DialogTitle>Редактировать партию</DialogTitle>
         </DialogHeader>
